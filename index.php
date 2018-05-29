7aca198c54c072737cc108caa8db03262526a4da
Alexander Schelchere <ascellie@gmail.com>
2018-05-29 02:33:56 +0000

    index.php - first variables. This is getting cool.

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My php website!</title>
+    </head>
+</html>
 
-echo "This is only the beginning!";
-
-?>
+<body>
+    
+    <center>
+        
+        <h1>
+               <?php 
+        // echo out some text~
+                echo "Here's a title!";
+                // Here's my first crack at making variables using PHP!
+                $first_string = "This is my first variable.";
+                
+                $first_number = 17;
+                
+                $second_number = 41;
+                
+                $third_number = $first_number;
+                
+                $fourth_number = $first_number + $second_number;
+                echo "
+                
+                ";
+                
+                echo $fourth_number;
+        
+                ?>  
+        </h1>
+       
+    </center>
+    
+</body>
