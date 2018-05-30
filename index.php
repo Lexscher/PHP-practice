7397f3b2c55cd58f6716087a5af000b1e9020910
Alexander Schelchere <ascellie@gmail.com>
2018-05-30 12:00:36 +0000

    For Loop FizzBuzz

--- index.php
+++ index.php
@@ -11,24 +11,20 @@
         
         <h1>
                <?php 
-                // While Loops!
-                
-                $a = 1;
-                
-                while( $a < 100 ){
-                    if ($a % 3 == 0 and $a % 5== 0 ) {
-                        echo "FizzBuzz";
-                   } elseif ($a % 3 == 0) {
-                        echo "Fizz";
-                    } elseif ($a % 5 == 0) {
-                        echo "Buzz";
-                    } else {
-                        echo $a;
-                    }
-                    echo "<br />";
-                    $a++;
-                }
+                // For Loops!
                
+               for ($a = 1; $a <= 100; $a++){
+                   if ($a % 3 == 0 and $a % 7 == 0) {
+                       echo "FizzBuzz";
+                   } elseif ($a % 3 == 0) {
+                      echo "Fizz";
+                   } elseif ($a % 7 == 0) {
+                       echo "Buzz";
+                   } else {
+                       echo $a;
+                   }
+                   echo "<br />";
+               }
                
                 ?>  
         </h1>
